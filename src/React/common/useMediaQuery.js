d0f1a44886c4d4a01971e89604c1059f255883fd
Valentina Artunduaga <vartunduaga1997@gmail.com>
2022-01-31 18:23:07 -0800

    Updating file

--- src/React/common/useMediaQuery.js
+++ src/React/common/useMediaQuery.js
@@ -6,21 +6,37 @@ import React, {
 } from 'react';
 
 export const defaultMediaQueries = {
+    // View Isolated
     xs: '(max-width: 320px)',
     sm: '(min-width: 321px) and (max-width: 720px)',
     md: '(min-width: 721px) and (max-width: 1024px)',
     lg: '(min-width: 1025px) and (max-width: 1900px)',
     xl: '(min-width: 1901px)',
+
+    // Cascading Up
+    smUp: `(min-width: 321px)`,
+    mdUp: `(min-width: 721px)`,
+    lgUp: `(min-width: 1025px)`,
+
+    // Device Orientation
     portrait: '(orientation: portrait)',
     landscape: '(orientation: landscape)',
 }
 
 const defaultState = {
     media: {
+        // View Isolated
         xs: true,
         sm: false,
         md: false,
         xl: false,
+
+        // Cascading Up
+        smUp: false,
+        mdUp: false,
+        lgUp: false,
+
+        // Device Orientation
         portrait: true,
         landscape: false,
     },
