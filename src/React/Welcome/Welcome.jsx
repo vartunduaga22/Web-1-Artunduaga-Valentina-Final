e7bc0bff8820e913bdbacfcd6fea20ad3a7b9ecb
Valentina Artunduaga <vartunduaga1997@gmail.com>
2022-02-10 20:33:48 -0800

    Adding Tabbed Components

--- src/React/Welcome/Welcome.jsx
+++ src/React/Welcome/Welcome.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import Tabbed from './Tabbed/Tabbed.jsx';
 import Staff from './Staff/Staff.jsx';
 
 const Welcome = () => {
@@ -8,6 +9,7 @@ const Welcome = () => {
     return (
         <WelcomeStyled className='Welcome'>
             <h1>Welcome</h1>
+            <Tabbed />
             <Staff />
         </WelcomeStyled>
     );
