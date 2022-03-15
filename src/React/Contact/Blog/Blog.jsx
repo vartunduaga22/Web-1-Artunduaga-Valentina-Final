a10a41884e883769fca6fa48f924a39da73d2630
Valentina Artunduaga <vartunduaga1997@gmail.com>
2022-03-14 17:43:30 -0700

    Changing CSS in Blogs

--- src/React/Contact/Blog/Blog.jsx
+++ src/React/Contact/Blog/Blog.jsx
@@ -18,13 +18,13 @@ export default Blog;
 const BlogStyled = styled.div`
     background-color: gray;
     margin: 20px 0px;
-    border: solid 2px teal;
+    border: solid 2px #212121;
 
     h3 {
         margin: 0px;
-        background-color: teal;
+        background-color: #212121;
         padding: 10px 20px;
-        color: white;
+        color: #e1e1e1;
     }
 
     .answer {
